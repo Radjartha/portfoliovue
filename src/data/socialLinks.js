b9bc228d329e26582ec3f674fed519472e2b9e3e
Radjartha <radjartha12@gmail.com>
2023-09-23 08:00:02 +0700

    first commit

---
+++ src/data/socialLinks.js
@@ -0,0 +1,29 @@
+export const socialLinks = [
+	{
+		id: 1,
+		name: 'Facebook',
+		icon: 'facebook',
+		url: 'https://www.facebook.com/profile.php?id=100075872207115',
+	},
+	{
+		id: 2,
+		name: 'GitHub',
+		icon: 'github',
+		url: 'https://github.com/radjartha',
+	},
+
+	{
+		id: 3,
+		name: 'LinkedIn',
+		icon: 'linkedin',
+		url: 'https://www.linkedin.com/in/radjartha',
+	},
+	{
+		id: 4,
+		name: 'Behance',
+		icon: 'globe',
+		url: 'https://www.behance.net/radjartha',
+	},
+
+
+];
